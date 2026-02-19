d0e81ce3485717b94b8127f846baa660e13e8780
BogdanDubenets <bogdan.dubenets@gmail.com>
2026-02-20 00:48:23 +0200

    fix: syntax error in BriefItem.tsx

--- pulse-miniapp/src/components/BriefItem.tsx
+++ pulse-miniapp/src/components/BriefItem.tsx
@@ -27,7 +27,9 @@ export const BriefItem: React.FC<BriefItemProps> = ({ item }) => {
                                     <a target="_blank" rel="noopener noreferrer" {...props} />
                                 )
                             }}
+                        >
                             {item.summary || ''}
+                        </Markdown>
                     </div>
                 </div>
                 {item.url && (
